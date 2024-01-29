cd177614cf39101842c6f9055853e8a9941b68e6
RdrgsAlex97 <alexandre-tavares@hotmail.com>
2024-01-29 04:10:36 +0000

    GameEngine and Inputs

--- Engine/GameEngine.h
+++ Engine/GameEngine.h
@@ -1,6 +1,15 @@
 #pragma once
-#include <memory>
 #include <string>
+#include <memory>
+
+class SDL_Renderer;
+union SDL_Event;
+class Manager;
+class SDLWrapper;
+class Window;
+class Renderer;
+class World;
+class Manager;
 
 class GameEngine
 {
@@ -10,53 +19,47 @@ public:
 
 	void Init(const char* windowTitle, int windowWidth, int windowHeight, bool isFullScreen);
 
-	int main() {
-		return 0;
-	}
-	
-//	//void Run();
-//
-//	//void Update();
-//
-//	//void HandleEvents();
-//
-//	void Render();
-//
-//	//void Shutdown();
-//
-//	//bool IsActive();
-//
-//	//static class GameEngine* GetEngine();
-//
-//	//World& GetWorld();
-//
-//	//inline float GetDeltaTime() { return deltaTime; }*/
-//
-//	static SDL_Renderer* GetRenderer();
-//	//static SDL_Event event;
-//	
-//private:
-//	void InitializeSDL();
-//	void InitializeWindow(const char* windowTitle, int windowWidth, int windowHeight, bool isFullScreen);
-//	void InitializeRenderer();
-//	void Cleanup();
-//
-//	SDLWrapper* m_sdl = nullptr;
-//	Window* m_window = nullptr;
-//	static Renderer* m_renderer;
-//
-//	std::unique_ptr<World> m_world;
-//
-//	bool m_isRunning;
-//	bool m_isActive;
-//
-//	static GameEngine* m_engine;
-//
-//	float frameStart;
-//	int currentTime;
-//	float frameTime;
-//	float deltaTime;
-//	float frameRate;
+	void Run();
 
+	void Update();
+
+	void HandleEvents();
+
+	void Render();
+
+	void Shutdown();
+
+	bool IsActive();
+
+	static class GameEngine* GetEngine();
+
+	World& GetWorld();
+
+	inline float GetDeltaTime() { return deltaTime; }
+
+	static SDL_Renderer* GetRenderer();
+	static SDL_Event event;
+
+private:
+	void InitializeSDL();
+	void InitializeWindow(const char* windowTitle, int windowWidth, int windowHeight, bool isFullScreen);
+	void InitializeRenderer();
+	void Cleanup();
+
+	SDLWrapper* m_sdl = nullptr;
+	Window* m_window = nullptr;
+	static Renderer* m_renderer;
+
+	std::unique_ptr<World> m_world;
+
+	bool m_isRunning;
+	bool m_isActive;
+
+	static GameEngine* m_engine;
+
+	float frameStart;
+	int currentTime;
+	float frameTime;
+	float deltaTime;
+	float frameRate;
 };
-
